6a7a04c6033367402ff75394951f586c641c7745
Adrian Tworek <62119460+AdrianTworek@users.noreply.github.com>
2026-06-28 02:10:18 +0200

    chore(main): release 1.3.0

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-const version = "1.2.0" // x-release-please-version
+const version = "1.3.0" // x-release-please-version
 
 func main() {
 	fmt.Printf("go-auth: v%s\n", version)
